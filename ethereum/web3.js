bbc39bbfef55efb68c70090942562e66fd480880
Chris <chris.netten@altrac.io>
2023-09-07 04:23:14 -0400

    Fix for if user is not using metamask

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,7 +1,18 @@
 import Web3 from "web3";
+require('dotenv').config({path:'../.env'})
+
+let web3;
  
-window.ethereum.request({ method: "eth_requestAccounts" });
- 
-const web3 = new Web3(window.ethereum);
+if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
+  // We are in the browser and metamask is running.
+  window.ethereum.request({ method: "eth_requestAccounts" });
+  web3 = new Web3(window.ethereum);
+} else {
+  // We are on the server *OR* the user is not running metamask
+  const provider = new Web3.providers.HttpProvider(
+    process.env.TEST_NETWORK
+  );
+  web3 = new Web3(provider);
+}
  
 export default web3;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import factory from '../ethereum/factory'
 
 export default function index() {
+
+    useEffect(() => {
+        async function fetchData() {
+        //   const campaigns = await factory.methods.getDeployedCampaigns().call();
+        }
+     
+        fetchData();
+      }, []);
+
   return (
     <div>index</div>
   )
